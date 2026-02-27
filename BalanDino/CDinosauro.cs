f7eefd04d5c27c2046ffc2d73410e2015953006b
BalanAntonio <12999441@itisrossi.vi.it>
2026-02-27 11:38:25 +0100

    Aggiungere i file di progetto.

---
+++ BalanDino/CDinosauro.cs
@@ -0,0 +1,21 @@
+﻿using BalanQueueStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanDino
+{
+    internal class CDinosauro
+    {
+        CQueue<string> coda;
+        CPila<string> pila;
+        int altezza;
+        
+        public CDinosauro(CQueue<string> c, CPila<string> p, int a)
+        {
+            coda = c; pila = p; altezza = a;
+        }
+    }
+}
---
+++ BalanDino/CRobot.cs
@@ -0,0 +1,28 @@
+﻿using BalanQueueStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanDino
+{
+    internal class CRobot
+    {
+        CQueue<string> coda;
+        SemaphoreSlim mutex;
+
+        public CRobot(CQueue<string> c, SemaphoreSlim s)
+        {
+            coda = c; mutex = mutex;
+        }
+
+        public async Task Lavora()
+        {
+            await Task.Delay(Random.Shared.Next(100, 1001));
+            await mutex.WaitAsync();
+            coda.Enqueue("qwertyuiop");
+            mutex.Release();
+        }
+    }
+}
